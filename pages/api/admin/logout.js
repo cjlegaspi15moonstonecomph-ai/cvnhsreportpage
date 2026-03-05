3f5d22757b6e62e82e0b7afe45ecdb4bc6043ab2
CJ Legaspi <cjlegaspi15moonstone.com.ph@gmail.com>
2026-03-05 21:32:19 +0800

    Initial commit

--- pages/api/admin/logout.js
+++
@@ -1,5 +0,0 @@
-// pages/api/admin/logout.js
-export default function handler(req, res) {
-  res.setHeader("Set-Cookie", `admin_token=; HttpOnly; Path=/; Max-Age=0; SameSite=Lax; Secure`);
-  return res.status(200).json({ success: true });
-}
